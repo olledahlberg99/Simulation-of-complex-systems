37bd9705de5d8dcbafbe876ed882d3557c674496
olledahlberg99 <77774782+olledahlberg99@users.noreply.github.com>
2021-11-24 15:23:24 +0100

    Add files via upload

--- HW3/11.2.py
+++ HW3/11.2.py
@@ -3,6 +3,7 @@ import random
 import matplotlib.pyplot as plt
 
 numberOfIndividuals = 1000
+iterations = 3
 numberOfLoops = 1000
 d = 0.8
 initialInfectionRate = 0.01
@@ -10,62 +11,65 @@ gamma = 0.01
 maxX = maxY = 100
 #1 = suceptible, 2 = infected, 3 = recovered
 recoveredList1 = []
-Beta = np.linspace(0.1,1,40)
+Beta = np.linspace(0.1,1,20)
 for beta in Beta:
-    individualPosition = []
-    individualStatus = []
-    for i in range(numberOfIndividuals):
-        individualPosition.append([random.randint(0,maxX), random.randint(0,maxY)])
-        individualStatus.append(1)
-
-    initialInfected = random.sample(range(numberOfIndividuals), round(numberOfIndividuals*initialInfectionRate))
-    for i in initialInfected:
-        individualStatus[i] = 2
-
-
-    for k in range(numberOfLoops):
+    it = 0
+    for a in range(iterations):
+        individualPosition = []
+        individualStatus = []
         for i in range(numberOfIndividuals):
-            r = random.random()
-            if r<d:
-                move = np.random.choice([1,2,3,4])
-                if move == 1:
-                    if individualPosition[i][0] < maxX:
-                        individualPosition[i][0] += 1
-                    else:
-                        individualPosition[i][0] = 0
-                elif move == 2:
-                    if individualPosition[i][0] > 0:
-                        individualPosition[i][0] -= 1
-                    else:
-                        individualPosition[i][0] = maxX
-                elif move == 3:
-                    if individualPosition[i][1] < maxY:
-                        individualPosition[i][1] += 1
-                    else:
-                        individualPosition[i][1] = 0
-                elif move == 4:
-                    if individualPosition[i][1] > 0:
-                        individualPosition[i][1] -= 1
-                    else:
-                        individualPosition[i][1] = maxY
+            individualPosition.append([random.randint(0,maxX), random.randint(0,maxY)])
+            individualStatus.append(1)
 
+        initialInfected = random.sample(range(numberOfIndividuals), round(numberOfIndividuals*initialInfectionRate))
+        for i in initialInfected:
+            individualStatus[i] = 2
+
+
+        for k in range(numberOfLoops):
+            for i in range(numberOfIndividuals):
+                r = random.random()
+                if r<d:
+                    move = np.random.choice([1,2,3,4])
+                    if move == 1:
+                        if individualPosition[i][0] < maxX:
+                            individualPosition[i][0] += 1
+                        else:
+                            individualPosition[i][0] = 0
+                    elif move == 2:
+                        if individualPosition[i][0] > 0:
+                            individualPosition[i][0] -= 1
+                        else:
+                            individualPosition[i][0] = maxX
+                    elif move == 3:
+                        if individualPosition[i][1] < maxY:
+                            individualPosition[i][1] += 1
+                        else:
+                            individualPosition[i][1] = 0
+                    elif move == 4:
+                        if individualPosition[i][1] > 0:
+                            individualPosition[i][1] -= 1
+                        else:
+                            individualPosition[i][1] = maxY
+
+            for i in range(numberOfIndividuals):
+                if individualStatus[i] == 2:
+                    indPos = individualPosition[i]
+                    if random.random() < beta:
+                        for j in range(numberOfIndividuals):
+                            if individualPosition[j] == indPos and individualStatus[j] == 1:
+                                individualStatus[j] = 2
+                    if random.random() < gamma:
+                        individualStatus[i] = 3
+            
+
+        NumberOfRecovered = 0
         for i in range(numberOfIndividuals):
-            if individualStatus[i] == 2:
-                indPos = individualPosition[i]
-                if random.random() < beta:
-                    for j in range(numberOfIndividuals):
-                        if individualPosition[j] == indPos and individualStatus[j] == 1:
-                            individualStatus[j] = 2
-                if random.random() < gamma:
-                    individualStatus[i] = 3
-        
-
-    NumberOfRecovered = 0
-    for i in range(numberOfIndividuals):
-        if individualStatus[i] == 3:
-            NumberOfRecovered += 1
-    recoveredList1.append(NumberOfRecovered)
-    print(NumberOfRecovered)
+            if individualStatus[i] == 3:
+                NumberOfRecovered += 1
+        it += NumberOfRecovered
+    recoveredList1.append(it/iterations)
+    print(it/iterations)
       
 
 
@@ -78,62 +82,65 @@ maxX = maxY = 100
 #1 = suceptible, 2 = infected, 3 = recovered
 
 recoveredList2 = []
-Beta = np.linspace(0.1,1,40)
+Beta = np.linspace(0.1,1,20)
 for beta in Beta:
-    individualPosition = []
-    individualStatus = []
-    for i in range(numberOfIndividuals):
-        individualPosition.append([random.randint(0,maxX), random.randint(0,maxY)])
-        individualStatus.append(1)
-
-    initialInfected = random.sample(range(numberOfIndividuals), round(numberOfIndividuals*initialInfectionRate))
-    for i in initialInfected:
-        individualStatus[i] = 2
-
-
-    for k in range(numberOfLoops):
+    it = 0
+    for a in range(iterations):
+        individualPosition = []
+        individualStatus = []
         for i in range(numberOfIndividuals):
-            r = random.random()
-            if r<d:
-                move = np.random.choice([1,2,3,4])
-                if move == 1:
-                    if individualPosition[i][0] < maxX:
-                        individualPosition[i][0] += 1
-                    else:
-                        individualPosition[i][0] = 0
-                elif move == 2:
-                    if individualPosition[i][0] > 0:
-                        individualPosition[i][0] -= 1
-                    else:
-                        individualPosition[i][0] = maxX
-                elif move == 3:
-                    if individualPosition[i][1] < maxY:
-                        individualPosition[i][1] += 1
-                    else:
-                        individualPosition[i][1] = 0
-                elif move == 4:
-                    if individualPosition[i][1] > 0:
-                        individualPosition[i][1] -= 1
-                    else:
-                        individualPosition[i][1] = maxY
+            individualPosition.append([random.randint(0,maxX), random.randint(0,maxY)])
+            individualStatus.append(1)
 
+        initialInfected = random.sample(range(numberOfIndividuals), round(numberOfIndividuals*initialInfectionRate))
+        for i in initialInfected:
+            individualStatus[i] = 2
+
+
+        for k in range(numberOfLoops):
+            for i in range(numberOfIndividuals):
+                r = random.random()
+                if r<d:
+                    move = np.random.choice([1,2,3,4])
+                    if move == 1:
+                        if individualPosition[i][0] < maxX:
+                            individualPosition[i][0] += 1
+                        else:
+                            individualPosition[i][0] = 0
+                    elif move == 2:
+                        if individualPosition[i][0] > 0:
+                            individualPosition[i][0] -= 1
+                        else:
+                            individualPosition[i][0] = maxX
+                    elif move == 3:
+                        if individualPosition[i][1] < maxY:
+                            individualPosition[i][1] += 1
+                        else:
+                            individualPosition[i][1] = 0
+                    elif move == 4:
+                        if individualPosition[i][1] > 0:
+                            individualPosition[i][1] -= 1
+                        else:
+                            individualPosition[i][1] = maxY
+
+            for i in range(numberOfIndividuals):
+                if individualStatus[i] == 2:
+                    indPos = individualPosition[i]
+                    if random.random() < beta:
+                        for j in range(numberOfIndividuals):
+                            if individualPosition[j] == indPos and individualStatus[j] == 1:
+                                individualStatus[j] = 2
+                    if random.random() < gamma:
+                        individualStatus[i] = 3
+            
+
+        NumberOfRecovered = 0
         for i in range(numberOfIndividuals):
-            if individualStatus[i] == 2:
-                indPos = individualPosition[i]
-                if random.random() < beta:
-                    for j in range(numberOfIndividuals):
-                        if individualPosition[j] == indPos and individualStatus[j] == 1:
-                            individualStatus[j] = 2
-                if random.random() < gamma:
-                    individualStatus[i] = 3
-        
-
-    NumberOfRecovered = 0
-    for i in range(numberOfIndividuals):
-        if individualStatus[i] == 3:
-            NumberOfRecovered += 1
-    recoveredList2.append(NumberOfRecovered)
-    print(NumberOfRecovered)
+            if individualStatus[i] == 3:
+                NumberOfRecovered += 1
+        it += NumberOfRecovered
+    recoveredList2.append(it/iterations)
+    print(it/iterations)
 
 plt.scatter(Beta, recoveredList1)
 plt.scatter(Beta, recoveredList2)
--- HW3/11.3.py
+++ HW3/11.3.py
@@ -4,69 +4,78 @@ import matplotlib.pyplot as plt
 
 numberOfIndividuals = 1000
 numberOfLoops = 1000
+iterations = 4
 d = 0.8
 initialInfectionRate = 0.01
 gamma = 0.01
 maxX = maxY = 100   
 beta = 0.4
-mu = 0.1
+Mu = np.linspace(0.001,0.02,50)
 #1 = suceptible, 2 = infected, 3 = recovered
-individualPosition = []
-individualStatus = []
+
 deadList = []
 StatusList = np.zeros((3,numberOfLoops))
+for a in range(len(Mu)):
+    it = 0
+    for b in range(iterations):
+        mu = Mu[a]
+        individualPosition = []
+        individualStatus = []
+        StatusList = np.zeros((3,numberOfLoops))
+        for i in range(numberOfIndividuals):
+            individualPosition.append([random.randint(0,maxX), random.randint(0,maxY)])
+            individualStatus.append(1)
 
-for i in range(numberOfIndividuals):
-    individualPosition.append([random.randint(0,maxX), random.randint(0,maxY)])
-    individualStatus.append(1)
-
-initialInfected = random.sample(range(numberOfIndividuals), round(numberOfIndividuals*initialInfectionRate))
-for i in initialInfected:
-    individualStatus[i] = 2
-
-
-for k in range(numberOfLoops):
-    for i in range(numberOfIndividuals):
-        r = random.random()
-        if r<d:
-            move = np.random.choice([1,2,3,4])
-            if move == 1:
-                if individualPosition[i][0] < maxX:
-                    individualPosition[i][0] += 1
-                else:
-                    individualPosition[i][0] = 0
-            elif move == 2:
-                if individualPosition[i][0] > 0:
-                    individualPosition[i][0] -= 1
-                else:
-                    individualPosition[i][0] = maxX
-            elif move == 3:
-                if individualPosition[i][1] < maxY:
-                    individualPosition[i][1] += 1
-                else:
-                    individualPosition[i][1] = 0
-            elif move == 4:
-                if individualPosition[i][1] > 0:
-                    individualPosition[i][1] -= 1
-                else:
-                    individualPosition[i][1] = maxY
-
-    for i in range(numberOfIndividuals):
-        if individualStatus[i] == 2:
-            indPos = individualPosition[i]
-            if random.random() < beta:
-                for j in range(numberOfIndividuals):
-                    if individualPosition[j] == indPos and individualStatus[j] == 1:
-                        individualStatus[j] = 2
-            if random.random() < gamma:
-                individualStatus[i] = 3
-            if random.random() < mu:
-                individualStatus[i] = 4
-    
-NumberOfDead = 0
-for i in range(numberOfIndividuals):
-    if individualStatus[i] == 4:
-        NumberOfDead += 1
-print(NumberOfDead)
+        initialInfected = random.sample(range(numberOfIndividuals), round(numberOfIndividuals*initialInfectionRate))
+        for i in initialInfected:
+            individualStatus[i] = 2
 
 
+        for k in range(numberOfLoops):
+            for i in range(numberOfIndividuals):
+                r = random.random()
+                if r<d:
+                    move = np.random.choice([1,2,3,4])
+                    if move == 1:
+                        if individualPosition[i][0] < maxX:
+                            individualPosition[i][0] += 1
+                        else:
+                            individualPosition[i][0] = 0
+                    elif move == 2:
+                        if individualPosition[i][0] > 0:
+                            individualPosition[i][0] -= 1
+                        else:
+                            individualPosition[i][0] = maxX
+                    elif move == 3:
+                        if individualPosition[i][1] < maxY:
+                            individualPosition[i][1] += 1
+                        else:
+                            individualPosition[i][1] = 0
+                    elif move == 4:
+                        if individualPosition[i][1] > 0:
+                            individualPosition[i][1] -= 1
+                        else:
+                            individualPosition[i][1] = maxY
+
+            for i in range(numberOfIndividuals):
+                if individualStatus[i] == 2:
+                    indPos = individualPosition[i]
+                    if random.random() < beta:
+                        for j in range(numberOfIndividuals):
+                            if individualPosition[j] == indPos and individualStatus[j] == 1:
+                                individualStatus[j] = 2
+                    if random.random() < gamma:
+                        individualStatus[i] = 3
+                    if random.random() < mu:
+                        individualStatus[i] = 4
+            
+        NumberOfDead = 0
+        for i in range(numberOfIndividuals):
+            if individualStatus[i] == 4:
+                NumberOfDead += 1
+        it += NumberOfDead
+    deadList.append(it/iterations)
+    print(it/iterations)
+
+plt.plot(Mu,deadList)
+plt.show()
