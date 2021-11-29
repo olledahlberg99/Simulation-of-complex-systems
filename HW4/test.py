aff5af441595bb1f5d530a4235c48cd7324fea5c
olledahlberg99 <77774782+olledahlberg99@users.noreply.github.com>
2021-11-29 20:48:07 +0100

    Add files via upload

--- HW4/test.py
+++ HW4/test.py
@@ -1,18 +1,4 @@
-import numpy as np
-N = 16
-lista = np.array([11, 2, 5, 3, 5, 3, 10, 0])
+a = [1,2,3,4,5,6,7,8]
+print(a[-1])
 
-# for k in range(N):
-#     [index] = np.where(lista == k)
-#     print(index)
-#     print(len(index))
-#     for i in range(len(index)-1):
-#         tempvector = [j for j in range(index[i],index[i+1])]
-#         lista = np.delete(lista,tempvector)
-
-# exists = 2 in lista
-# print(exists)
-a = [[1,2,3],[4,5,6]]
-b = np.multiply(a,6)
-print(b)
 
